2e4d4387c1d9f1ecbc60d1945e48bdec01b8eab5
Patryk Robakowski <patryk711@outlook.com>
2018-12-18 10:44:55 +0100

    diffirent approach to connect with google to allow for more than one user. index.html created but not finished yet

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,44 @@
+const CLIENT_ID = "294307018578-mlelfvhktca0k84t1brnho8ssn25dsqe.apps.googleusercontent.com";
+
+
+function start() {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+            client_id: CLIENT_ID,
+            scope: "https://www.googleapis.com/auth/calendar.events"
+        });
+    });
+}
+
+$('#signInButton').click(function() {
+// signInCallback defined in step 6.
+    auth2.grantOfflineAccess().then(signInCallback);
+});
+
+function signInCallback(authResult) {
+    console.log('authResult', authResult);
+    if (authResult['code']) {
+
+        // Hide the sign-in button now that the user is authorized, for example:
+        $('#signInButton').attr('style', 'display: none');
+
+        // Send the code to the server
+        $.ajax({
+            type: 'POST',
+            url: 'https://localhost:8080/storeauthcode',
+            // Always include an `X-Requested-With` header in every AJAX request,
+            // to protect against CSRF attacks.
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            contentType: 'application/octet-stream; charset=utf-8',
+            success: function(result) {
+                // Handle or verify the server response.
+            },
+            processData: false,
+            data: authResult['code']
+        });
+    } else {
+        // There was an error.
+    }
+}
