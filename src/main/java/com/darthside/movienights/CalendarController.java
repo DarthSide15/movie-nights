cf9054a411d124ab18188c45410842bbb11adfdb
Patryk Robakowski <patryk711@outlook.com>
2019-01-12 18:19:45 +0100

    Refactoring in CalendarController

--- src/main/java/com/darthside/movienights/CalendarController.java
+++ src/main/java/com/darthside/movienights/CalendarController.java
@@ -15,10 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -28,18 +25,21 @@ public class CalendarController {
     @Autowired
     TokenTable tokenTable;
 
+    // Needed to handle both standard and whole-day events
     public static <T> T firstNonNull(T... params) {
         for (T param : params)
             if (param != null)
                 return param;
         return null;
     }
-
+    
     @RequestMapping(value = "/periods", method = RequestMethod.GET)
     public List<Period> getPossiblePeriods() {
+
         List<Token> tokens = tokenTable.findAll();
         List<Event> allEvents = new ArrayList<>();
 
+        // Gets events from all available users and stores them in 'allEvents'
         for (Token t: tokens) {
             // Use an accessToken previously gotten to call Google's API
             GoogleCredential credential = new GoogleCredential().setAccessToken(t.getAccessToken());
@@ -48,13 +48,7 @@ public class CalendarController {
                             .setApplicationName("Movie Nights")
                             .build();
 
-            // List the next 10 events from the primary calendar.
-            // Instead of printing these with System out, you should of course store them in a database or in-memory variable to use for your application.
-
-    /*
-      The most important parts are
-        event.getSummary() // Title of calendar event
-    */
+            // Gets the next 10 events from the primary calendar
             DateTime now = new DateTime(System.currentTimeMillis());
             Events events = null;
             try {
@@ -72,10 +66,8 @@ public class CalendarController {
             allEvents.addAll(items);
         }
 
-        System.out.println(allEvents);
         List<Period> periods = new ArrayList<>();
 
-        // TODO: Calculate available periods
         // Sort 'allEvents' List in chronological order
         for (Event event : allEvents) {
             DateTime start = firstNonNull(event.getStart().getDateTime(), event.getStart().getDate());
@@ -83,14 +75,14 @@ public class CalendarController {
         }
         allEvents.sort(Comparator.comparing(event -> firstNonNull(event.getStart().getDateTime(), event.getStart().getDate()).getValue()));
 
-        // Now - Start time of first event = Available period
+        // Calculate period Now to start of first event
         Period nowToNext = new Period(
                 new DateTime(System.currentTimeMillis()),
                 firstNonNull(allEvents.get(0).getStart().getDateTime(), allEvents.get(0).getStart().getDate())
         );
         periods.add(nowToNext);
 
-        // End time of first event - Start time of next event = Available period
+        // Calculate periods from end time of previous event to start time of next event
         for (int i = 1; i < allEvents.size() - 1; i++) {
             Period periodBetweenEvents = new Period(firstNonNull(allEvents.get(i).getEnd().getDateTime(), allEvents.get(i).getEnd().getDate()),
                     firstNonNull(allEvents.get(i + 1).getStart().getDateTime(), allEvents.get(i + 1).getStart().getDate()));
@@ -98,7 +90,7 @@ public class CalendarController {
         }
         System.out.println(periods);
 
-        // End time of last event - Forever = Available Period
+        // Calculate period from end time of last event in the list plus 31 days
         Period lastEventPlusAMonth = new Period(
                 firstNonNull(allEvents.get(allEvents.size() -1).getEnd().getDateTime(), allEvents.get(allEvents.size() - 1).getEnd().getDate()),
                 new DateTime(  firstNonNull(allEvents.get(allEvents.size() -1).getEnd().getDateTime(), allEvents.get(allEvents.size() - 1).getEnd().getDate()).getValue()
@@ -106,30 +98,6 @@ public class CalendarController {
         );
         periods.add(lastEventPlusAMonth);
 
-        // TODO: Store available periods in 'periods' List
-
         return periods;
-
-        /*
-            if (items.isEmpty()) {
-                System.out.println("No upcoming events found.");
-            } else {
-                System.out.println("Upcoming events");
-                for (Event event : items) {
-
-                    DateTime start = event.getStart().getDateTime();
-                    if (start == null) { // If it's an all-day-event - store the date instead
-                        start = event.getStart().getDate();
-                    }
-                    DateTime end = event.getEnd().getDateTime();
-                    if (end == null) { // If it's an all-day-event - store the date instead
-                        end = event.getStart().getDate();
-                    }
-                    System.out.printf("%s (%s) -> (%s)\n", event.getSummary(), start, end);
-                }
-            }
-                */
-
-
     }
 }
